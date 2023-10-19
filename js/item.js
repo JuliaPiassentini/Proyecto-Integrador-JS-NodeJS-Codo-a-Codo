a8652c84d8327460105e55416059b5d892c70aa0
johnwickdesarrollador1 <143667159+johnwickdeveloper1@users.noreply.github.com>
2023-10-19 19:42:15 -0300

    first commit
    Implementación de slider en las páginas de home a ítem. Funcionalidad de los botones +/- en ítem

---
+++ js/item.js
@@ -0,0 +1,9 @@
+const add = document.querySelector('#add');
+const subtract = document.querySelector('#subtract');
+const quantity = document.querySelector('#quantity');
+
+add.addEventListener('click', () => quantity.value = Number(quantity.value) + 1);
+subtract.addEventListener('click', () => {
+    if (Number(quantity.value)>0)
+    quantity.value = Number(quantity.value) - 1
+});
